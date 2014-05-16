985eaa4da84f9227325b9cfe2838308570b0f30f
Yazid <Alabaster456@users.noreply.github.com>
2014-05-16 13:37:32 -0700

    Create jQuery.js

---
+++ jQuery.js
@@ -0,0 +1,210 @@
+$(document).ready(function(){
+$("#expand").mouseenter(function(){
+$("#expand").css("font-size", "150px");
+  
+  var letterA = new Audio("http://soundfxcenter.com/human/speech/90a978_Letter_A_Sound_Effect.mp3");
+  letterA.play();
+
+});
+  $("#expand").mouseleave(function(){
+$("#expand").css("font-size", "120px");
+  });
+  $("#expand1").mouseenter(function(){
+    $("#expand1").css("font-size", "150px");
+ var letterB = new Audio("http://soundfxcenter.com/human/speech/d75dd5_Letter_B_Sound_Effect.mp3");
+    letterB.play();
+  });
+   $("#expand1").mouseleave(function(){
+$("#expand1").css("font-size", "120px");
+  });
+  $("#expand2").mouseenter(function(){
+    $("#expand2").css("font-size", "150px");
+  
+  var letterC = new Audio("http://soundfxcenter.com/human/speech/018b48_Letter_C_Sound_Effect.mp3");
+  letterC.play();
+  });
+   $("#expand2").mouseleave(function(){
+$("#expand2").css("font-size", "120px");
+  });
+  $("#expand3").mouseenter(function(){
+  $("#expand3").css("font-size", "150px");
+    var letterD = new Audio("http://soundfxcenter.com/human/speech/b5b6fe_Letter_D_Sound_Effect.mp3");
+  letterD.play();
+  });
+    $("#expand3").mouseleave(function(){
+$("#expand3").css("font-size", "120px");
+  });
+  $("#expand4").mouseenter(function(){
+ $("#expand4").css("font-size", "150px");
+    var letterE = new Audio("http://soundfxcenter.com/human/speech/7f373d_Letter_E_Sound_Effect.mp3");
+  letterE.play();
+  });
+    $("#expand4").mouseleave(function(){
+$("#expand4").css("font-size", "120px");
+  });
+   $("#expand5").mouseenter(function(){
+  $("#expand5").css("font-size", "150px");
+  var letterF = new Audio("http://soundfxcenter.com/human/speech/93d6ce_Letter_F_Sound_Effect.mp3");
+  letterF.play();
+   });
+     $("#expand5").mouseleave(function(){
+$("#expand5").css("font-size", "120px");
+  });
+  $("#expand6").mouseenter(function(){
+     $("#expand6").css("font-size", "150px");
+  
+  var letterG = new Audio("http://soundfxcenter.com/human/speech/cc32a4_Letter_G_Sound_Effect.mp3");
+  letterG.play();
+  });
+  $("#expand6").mouseleave(function(){
+$("#expand6").css("font-size", "120px");
+  });
+   $("#expand7").mouseenter(function(){
+     $("#expand7").css("font-size", "150px");
+  var letterH = new Audio("http://soundfxcenter.com/human/speech/f5252c_Letter_H_Sound_Effect.mp3");
+  letterH.play();
+   });
+  $("#expand7").mouseleave(function(){
+$("#expand7").css("font-size", "120px");
+  });
+   $("#expand8").mouseenter(function(){
+     $("#expand8").css("font-size", "150px");
+  var letterI = new Audio("http://soundfxcenter.com/human/speech/f7b4f6_Letter_I_Sound_Effect.mp3");
+  letterI.play();
+   });
+    $("#expand8").mouseleave(function(){
+$("#expand8").css("font-size", "120px");
+  });
+   $("#expand9").mouseenter(function(){
+     $("#expand9").css("font-size", "150px");
+  var letterJ = new Audio("http://soundfxcenter.com/video-games/mortal-kombat-4/e7c4b0_Mortal_Kombat_4_Letter_J_Sound_Effect.mp3");
+  letterJ.play();
+   });
+    $("#expand9").mouseleave(function(){
+$("#expand9").css("font-size", "120px");
+  });
+       $("#expand10").mouseenter(function(){
+     $("#expand10").css("font-size", "150px");
+  var letterK = new Audio("http://soundfxcenter.com/human/speech/d7ec8a_Letter_K_Sound_Effect.mp3");
+  letterK.play();
+   }); $("#expand10").mouseleave(function(){
+$("#expand10").css("font-size", "120px");
+  });
+  $("#expand11").mouseenter(function(){
+     $("#expand11").css("font-size", "150px");
+  var letterL = new Audio("http://soundfxcenter.com/video-games/mortal-kombat-4/3c72e0_Mortal_Kombat_4_Letter_L_Sound_Effect.mp3");
+  letterL.play();
+   }); $("#expand11").mouseleave(function(){
+$("#expand11").css("font-size", "120px");
+  });
+    $("#expand12").mouseenter(function(){
+     $("#expand12").css("font-size", "150px");
+  var letterM = new Audio("http://soundfxcenter.com/human/speech/7bb1db_Letter_M_Sound_Effect.mp3");
+  letterM.play();
+   }); $("#expand12").mouseleave(function(){
+$("#expand12").css("font-size", "120px");
+  });
+  $("#expand13").mouseenter(function(){
+     $("#expand13").css("font-size", "150px");
+  var letterN = new Audio("http://soundfxcenter.com/human/speech/617411_Letter_N_Sound_Effect.mp3");
+  letterN.play();
+   }); $("#expand13").mouseleave(function(){
+$("#expand13").css("font-size", "120px");
+  });
+    $("#expand14").mouseenter(function(){
+     $("#expand14").css("font-size", "150px");
+  var letterO = new Audio("http://soundfxcenter.com/human/speech/a72487_Letter_O_Sound_Effect.mp3");
+
+  letterO.play();
+   }); $("#expand14").mouseleave(function(){
+$("#expand14").css("font-size", "120px");
+  });
+  $("#expand15").mouseenter(function(){
+     $("#expand15").css("font-size", "150px");
+  var letterP = new Audio("http://soundfxcenter.com/human/speech/b35e61_Letter_P_Sound_Effect.mp3");
+
+  letterP.play();
+   }); $("#expand15").mouseleave(function(){
+$("#expand15").css("font-size", "120px");
+  });
+   $("#expand16").mouseenter(function(){
+     $("#expand16").css("font-size", "150px");
+  var letterQ = new Audio("http://soundfxcenter.com/video-games/mortal-kombat-4/57a1cd_Mortal_Kombat_4_Letter_Q_Sound_Effect.mp3");
+
+  letterQ.play();
+   }); $("#expand16").mouseleave(function(){
+$("#expand16").css("font-size", "120px");
+  });
+     $("#expand17").mouseenter(function(){
+     $("#expand17").css("font-size", "150px");
+  var letterR = new Audio("http://soundfxcenter.com/human/speech/ba5e2c_Letter_R_Sound_Effect.mp3");
+
+  letterR.play();
+   }); $("#expand17").mouseleave(function(){
+$("#expand17").css("font-size", "120px");
+  });
+  $("#expand18").mouseenter(function(){
+     $("#expand18").css("font-size", "150px");
+  var letterS = new Audio("http://soundfxcenter.com/video-games/mortal-kombat-4/748979_Mortal_Kombat_4_Letter_S_Sound_Effect.mp3");
+
+  letterS.play();
+   }); $("#expand18").mouseleave(function(){
+$("#expand18").css("font-size", "120px");
+  });
+   $("#expand19").mouseenter(function(){
+     $("#expand19").css("font-size", "150px");
+  var letterT = new Audio("http://soundfxcenter.com/human/speech/6592a9_Letter_T_Sound_Effect.mp3");
+
+  letterT.play();
+   }); $("#expand19").mouseleave(function(){
+$("#expand19").css("font-size", "120px");
+  });
+    $("#expand20").mouseenter(function(){
+     $("#expand20").css("font-size", "150px");
+  var letterU = new Audio("http://soundfxcenter.com/video-games/mortal-kombat-4/dd3c23_Mortal_Kombat_4_Letter_U_Sound_Effect.mp3");
+
+  letterU.play();
+   }); $("#expand20").mouseleave(function(){
+$("#expand20").css("font-size", "120px");
+  });
+  $("#expand21").mouseenter(function(){
+     $("#expand21").css("font-size", "150px");
+  var letterV = new Audio("http://soundfxcenter.com/video-games/mortal-kombat-4/eaa475_Mortal_Kombat_4_Letter_V_Sound_Effect.mp3");
+
+  letterV.play();
+   }); $("#expand21").mouseleave(function(){
+$("#expand21").css("font-size", "120px");
+  });
+   $("#expand22").mouseenter(function(){
+     $("#expand22").css("font-size", "150px");
+  var letterW = new Audio("http://soundfxcenter.com/human/speech/53e5c5_Letter_W_Sound_Effect.mp3");
+
+  letterW.play();
+   }); $("#expand22").mouseleave(function(){
+$("#expand22").css("font-size", "120px");
+  });
+     $("#expand23").mouseenter(function(){
+     $("#expand23").css("font-size", "150px");
+  var letterX = new Audio("http://soundfxcenter.com/human/speech/776858_Letter_X_Sound_Effect.mp3");
+
+  letterX.play();
+   }); $("#expand23").mouseleave(function(){
+$("#expand23").css("font-size", "120px");
+  });
+  $("#expand24").mouseenter(function(){
+     $("#expand24").css("font-size", "150px");
+  var letterY = new Audio("http://soundfxcenter.com/video-games/mortal-kombat-4/da3ef2_Mortal_Kombat_4_Letter_Y_Sound_Effect.mp3");
+
+  letterY.play();
+   }); $("#expand24").mouseleave(function(){
+$("#expand24").css("font-size", "120px");
+  });
+   $("#expand25").mouseenter(function(){
+     $("#expand25").css("font-size", "150px");
+  var letterZ = new Audio("http://soundfxcenter.com/human/speech/152314_Letter_Z_Sound_Effect.mp3");
+
+  letterZ.play();
+   }); $("#expand25").mouseleave(function(){
+$("#expand25").css("font-size", "120px");
+  });
+  });
